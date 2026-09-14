369254e11e5a62dbac9d5a76fe716c0c56675f11
AmberCheng0806 <qaz29zaq34@gmail.com>
2026-09-14 15:41:35 +0800

    新增Core層AutoComplete Presenter實作

---
+++ GoogleMapSDK.Core/Components/AutoComplete/GoogleMapPresenter.cs
@@ -0,0 +1,36 @@
+﻿using GoogleMapSDK.Contract.Contracts.API;
+using GoogleMapSDK.Contract.Models.AutoComplete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleMapSDK.Core.Components.AutoComplete
+{
+    public class GoogleMapPresenter : IAutoCompletePresenter<Place>
+    {
+        private IGoogleMapAPIContext GoogleMapContext;
+
+        public GoogleMapPresenter(IGoogleMapAPIContext googleMapAPIContext)
+        {
+            GoogleMapContext = googleMapAPIContext;
+        }
+        public async Task<IEnumerable<Place>> GetSearchResults(string inputText)
+        {
+            var autoComplete = await GoogleMapContext.PlaceContext.AutoCompleteAsync(inputText);
+            var list = autoComplete.suggestions.Select(async x =>
+            {
+                var searchText = await GoogleMapContext.PlaceContext.SearchTextAsync(x.placePrediction.text.text);
+                return new Place
+                {
+                    Name = searchText.places[0].displayName.text,
+                    Value = searchText.places[0].id
+                };
+            }).ToList();
+            var result = await Task.WhenAll(list);
+            return result;
+        }
+    }
+}
---
+++ GoogleMapSDK.Core/Components/AutoComplete/YoutubePresenter.cs
@@ -0,0 +1,27 @@
+﻿using GoogleMapSDK.Contract.Contracts.API;
+using GoogleMapSDK.Contract.Models.AutoComplete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeAPI.Interfaces;
+
+namespace GoogleMapSDK.Core.Components.AutoComplete
+{
+    public class YoutubePresenter : IAutoCompletePresenter<Video>
+    {
+        private IYoutubeContext YoutubeContext;
+        public YoutubePresenter(IYoutubeContext youtubeContext)
+        {
+            YoutubeContext = youtubeContext;
+        }
+
+        public async Task<IEnumerable<Video>> GetSearchResults(string inputText)
+        {
+            var search = await YoutubeContext.Search.GetAllAsync(inputText, 0, default);
+            return search.items.Select(x => new Video(x.snippet.title, x.id.videoId)).ToList();
+        }
+    }
+}
