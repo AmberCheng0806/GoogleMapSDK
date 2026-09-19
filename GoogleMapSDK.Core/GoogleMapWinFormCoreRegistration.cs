e0fc44937f5d7a7204fd82de9767dcdf06ee9b9e
AmberCheng0806 <qaz29zaq34@gmail.com>
2026-09-20 00:21:34 +0800

    完成Core層服務註冊與相依設定

---
+++ GoogleMapSDK.Core/GoogleMapWinFormCoreRegistration.cs
@@ -0,0 +1,28 @@
+﻿using GoogleMapSDK.Contract.Contracts.API;
+using GoogleMapSDK.Contract.Models.AutoComplete;
+using GoogleMapSDK.Contract.Options;
+using GoogleMapSDK.Core.Components.AutoComplete;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using YoutubeAPI;
+using YoutubeAPI.Interfaces;
+
+namespace GoogleMapSDK.Core
+{
+    public static class GoogleMapWinFormCoreRegistration
+    {
+        public static IServiceCollection AddGoogleMapWinFormCoreService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<IAutoCompletePresenter<Place>, GoogleMapPresenter>();
+            services.AddSingleton<IAutoCompletePresenter<Video>, YoutubePresenter>();
+            services.AddSingleton<IYoutubeContext, YoutubeContext>();
+            return services;
+        }
+    }
+}
