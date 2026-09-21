da3b647914c227bfeeddb1ec17b5f06e2c38f8cb
AmberCheng0806 <qaz29zaq34@gmail.com>
2026-09-21 16:37:42 +0800

    新增WPF AutoComplete測試專案

---
+++ GoogleMapSDK.UI.WPF.Test/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Windows;
+using GoogleMapSDK.API;
+using GoogleMapSDK.Core;
+
+namespace GoogleMapSDK.UI.WPF.Test
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+            var service = new IoC_Container.ServiceCollection();
+            service.AddGoogleMapWPFService(config);
+            service.AddGoogleMapWinFormCoreService(config);
+            service.AddGoogleMapAPIService(config);
+            service.AddSingleton<MainWindow, MainWindow>();
+
+            var provider = service.BuildServiceProvider();
+            var mainWindow = (MainWindow)provider.GetService(typeof(MainWindow));
+            mainWindow.Show();
+        }
+    }
+
+}
---
+++ GoogleMapSDK.UI.WPF.Test/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using GoogleMapSDK.Contract.Models.AutoComplete;
+using GoogleMapSDK.UI.WPF.Components.AutoComplete;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GoogleMapSDK.UI.WPF.Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow(BaseAutoComplete<Place> origin, BaseAutoComplete<Video> videoTest)
+        {
+            InitializeComponent();
+            origin.OnSelectedItem += OnSelectedItem;
+            videoTest.OnSelectedItem += OnSelectedItem;
+            AutoCompletePanel.Children.Add(origin);
+            AutoCompletePanel.Children.Add(videoTest);
+
+        }
+        private void OnSelectedItem(object? sender, Video e)
+        {
+            MessageBox.Show(e.Title);
+        }
+
+        private void OnSelectedItem(object? sender, Place e)
+        {
+            MessageBox.Show(e.Name);
+        }
+    }
+}
