383b2e9e0dbbafd82c452afced24415e427c5822
AmberCheng0806 <qaz29zaq34@gmail.com>
2026-09-21 16:36:01 +0800

    新增WPF AutoComplete服務註冊

---
+++ GoogleMapSDK.UI.WPF/GoogleMapWPFRegistration.cs
@@ -0,0 +1,23 @@
+﻿using GoogleMapSDK.Contract.Models.AutoComplete;
+using GoogleMapSDK.UI.WPF.Components.AutoComplete;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleMapSDK.UI.WPF
+{
+    public static class GoogleMapWPFRegistration
+    {
+        public static IServiceCollection AddGoogleMapWPFService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<BaseAutoComplete<Place>, GoogleMapAutoCompleteTextBox>();
+            services.AddSingleton<BaseAutoComplete<Video>, YoutubeAutoCompleteTextBox>();
+
+            return services;
+        }
+    }
+}
