2f9a0c8aa211285b9934e590c6cd30201e94302d
AmberCheng0806 <qaz29zaq34@gmail.com>
2026-09-21 16:21:23 +0800

    建立WPF AutoComplete基底元件

---
+++ GoogleMapSDK.UI.WPF/Components/AutoComplete/AutoCompleteTextBox.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GoogleMapSDK.UI.WPF.Components.AutoComplete
+{
+    /// <summary>
+    /// AutoCompleteTextBox.xaml 的互動邏輯
+    /// </summary>
+    public partial class AutoCompleteTextBox : UserControl
+    {
+        public AutoCompleteTextBox()
+        {
+            InitializeComponent();
+        }
+
+        public Visibility IsDropDownVisible
+        {
+            get => (Visibility)GetValue(IsDropDownVisibleProperty);
+            set => SetValue(IsDropDownVisibleProperty, value);
+        }
+
+        public static readonly DependencyProperty IsDropDownVisibleProperty =
+            DependencyProperty.Register(
+                nameof(IsDropDownVisible),
+                typeof(Visibility),
+                typeof(AutoCompleteTextBox),
+                new PropertyMetadata(Visibility.Collapsed));
+
+    }
+}
+
