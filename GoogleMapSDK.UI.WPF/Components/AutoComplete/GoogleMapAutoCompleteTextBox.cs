3e7ed9975df263b3cd2598dbcfd89f199aad13d4
AmberCheng0806 <qaz29zaq34@gmail.com>
2026-09-21 16:24:15 +0800

    實作WPF Google Map與YouTube AutoComplete元件

---
+++ GoogleMapSDK.UI.WPF/Components/AutoComplete/GoogleMapAutoCompleteTextBox.cs
@@ -0,0 +1,27 @@
+﻿using GoogleMapSDK.Contract.Contracts.API;
+using GoogleMapSDK.Contract.Models.AutoComplete;
+using GoogleMapSDK.Core.Components.AutoComplete;
+using IoC_Container.Factory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleMapSDK.UI.WPF.Components.AutoComplete
+{
+    public class GoogleMapAutoCompleteTextBox : BaseAutoComplete<Place>
+    {
+        private IAutoCompletePresenter<Place> AutoCompletePresenter;
+        public override string DisplayMember => "Name";
+        public override string ValueMember => "Value";
+        public GoogleMapAutoCompleteTextBox(IPresenterFactory presenterFactory)
+        {
+            AutoCompletePresenter = presenterFactory.Create<IAutoCompletePresenter<Place>>(this);
+        }
+        public override async Task<IEnumerable<Place>> GetSearchResults(string inputText)
+        {
+            return await AutoCompletePresenter.GetSearchResults(inputText);
+        }
+    }
+}
---
+++ GoogleMapSDK.UI.WPF/Components/AutoComplete/YoutubeAutoCompleteTextBox.cs
@@ -0,0 +1,27 @@
+﻿using GoogleMapSDK.Contract.Contracts.API;
+using GoogleMapSDK.Contract.Models.AutoComplete;
+using GoogleMapSDK.Core.Components.AutoComplete;
+using IoC_Container.Factory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleMapSDK.UI.WPF.Components.AutoComplete
+{
+    public class YoutubeAutoCompleteTextBox : BaseAutoComplete<Video>
+    {
+        private IAutoCompletePresenter<Video> AutoCompletePresenter;
+        public override string DisplayMember => "Title";
+        public override string ValueMember => "VideoId";
+        public YoutubeAutoCompleteTextBox(IPresenterFactory presenterFactory)
+        {
+            AutoCompletePresenter = presenterFactory.Create<IAutoCompletePresenter<Video>>(this);
+        }
+        public override async Task<IEnumerable<Video>> GetSearchResults(string inputText)
+        {
+            return await AutoCompletePresenter.GetSearchResults(inputText);
+        }
+    }
+}
